1ea0db43c18295a3e341d3f000426ae30c82d9d1
naila-ady <bholoady@gmail.com>
2024-04-17 18:16:47 +0500

    complete

---
+++ index.ts
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+const randomNumber =Math.floor(Math.random()*6+1);
+console.log("Welcome to number Guessing Game!")
+const answers = await inquirer.prompt([
+    {
+        name:"userGuessedNumber",
+        type:"number",
+        message:"Please guess a number:",
+    }
+])
+if(answers.userGuessedNumber === randomNumber){
+console.log("Congratulations!You guessed the right number.");
+}else{
+    console.log("You guessed wrong number");
+}
